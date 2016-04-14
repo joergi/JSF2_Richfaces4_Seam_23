02f9cfb9505b10303b9aa833a9a5cf17a3603640
Jörg Liedl <j.liedl@dialogworks.de>
2016-04-14 15:54:18 +0200

    jsf2 with working richfaces 4 rich calendar

--- src/main/java/de/joergi/Hallo.java
+++ src/main/java/de/joergi/Hallo.java
@@ -1,6 +1,7 @@
 package de.joergi;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -12,6 +13,10 @@ public class Hallo implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private String input;
+	
+	private String color;
+	
+	private Date testDate;
 
 	public String getInput() {
 		return input;
@@ -20,5 +25,23 @@ public class Hallo implements Serializable {
 	public void setInput(String input) {
 		this.input = input;
 	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Date getTestDate() {
+		return testDate;
+	}
+
+	public void setTestDate(Date testDate) {
+		this.testDate = testDate;
+	}
+	
+	
 
 }
